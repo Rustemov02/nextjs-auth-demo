0bfc4f601bdb155bc1d20e988178bc5461b2b4ce
Bunyamin <bunyamin2002@mail.ru>
2025-02-05 05:51:25 +0400

    Revert "Add registration functionality; implement register thunk and update UI"
    This reverts commit 0fd2edf5d1970d7cde67a37bcc5fc6230f868acb.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { login, logout , register } from "@/store/auth/authSlice";
+import { login, logout } from "@/store/auth/authSlice";
 import { AppDispatch } from "@/store/store"; 
 import { useDispatch } from "react-redux";
 
@@ -16,16 +16,11 @@ export default function Home() {
   const handleLogout = async() => {
     await dispatch(logout())
   }
-  
-  const handleRegister = async () => {
-    await dispatch(register({username , password , email})).unwrap()
-  }
 
   return (
     <div className="flex flex-col">
       <button onClick={handleLogin}>Login</button>
       <button onClick={handleLogout}>Log Out</button>
-      <button onClick={handleRegister}>Register</button>
     </div>
   );
 }
--- src/store/auth/authSlice.ts
+++ src/store/auth/authSlice.ts
@@ -18,37 +18,6 @@ const initialState: AuthState = {
   error: null,
 };
 
-export const register = createAsyncThunk(
-  "auth/register",
-  async (
-    userData: { email: string; password: string; username: string },  
-    { rejectWithValue }
-  ) => {
-    try {
-      const response = await fetch(
-        "https://679b27d533d316846322e42b.mockapi.io/api/auth/userInfo",
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            email: userData.email,
-            password: userData.password,
-            username: userData.username,
-            accessToken: `fake-access-token-${Date.now()}`,
-          }),
-        }
-      );
-
-      if (!response.ok) throw new Error("Registration Failed !");
-
-      const data = await response.json();
-      return {username : data.username , email : data.email , password : data.password , accessToken : data.accessToken}
-    } catch (error: any) {
-      return rejectWithValue(error.message || "Registration error...");
-    }
-  }
-);
-
 export const login = createAsyncThunk(
   "auth/login",
   async (userData: { password: string }, { rejectWithValue }) => {
@@ -89,7 +58,6 @@ const authSlice = createSlice({
   initialState,
   reducers: {
     logout: (state) => {
-      console.log("logout...");
       state.user = null;
       state.accessToken = null;
       state.isAuthenticated = false;
@@ -98,7 +66,6 @@ const authSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder
-      // Login thunk
       .addCase(login.pending, (state) => {
         state.loading = true;
         state.error = null;
@@ -121,34 +88,7 @@ const authSlice = createSlice({
       .addCase(login.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload as string;
-        console.log("Hatali girish");
-      })
-
-      // Register thunk
-      .addCase(register.pending, (state) => {
-        state.loading = true;
-        state.error = null;
-      })
-      .addCase(
-        register.fulfilled,
-        (
-          state,
-          action: PayloadAction<{
-            username: string;
-            email: string;
-            password: string;
-            accessToken : string;
-          }>
-        ) => {
-          console.log(action.payload)
-          const data = action.payload
-          state.loading = false;
-          state.user = data.username
-          state.accessToken = data.accessToken
-          state.isAuthenticated = true
-          localStorage.setItem("authState" , JSON.stringify(data))
-        }
-      );
+      });
   },
 });
 
